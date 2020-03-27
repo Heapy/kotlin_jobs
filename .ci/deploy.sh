c09b45141ac096ed393d1ffa8a3ada49c272f82e
Ruslan Ibragimov <ruslan@ibragimov.by>
2020-03-28 01:07:51 +0300

    Change target for deploy

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -4,7 +4,7 @@ set -e # exit with nonzero exit code if anything fails
 echo "TRAVIS_BUILD_DIR=${TRAVIS_BUILD_DIR}"
 
 echo "Add ssh key to remote server..."
-openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in deploy@morty.enc -out ~/.ssh/deploy@morty -d
+openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in ./.ci/deploy@morty.enc -out ~/.ssh/deploy@morty -d
 chmod 600 ~/.ssh/deploy@morty
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/deploy@morty
